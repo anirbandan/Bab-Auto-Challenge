1cf913617e72a7e7e5fd32507d2d1a9084e716a4
adan <Viv_020922200791>
2020-09-23 17:33:41 +0200

    Created my account page to create objects to assert successful login on creating an account
    Added maven dependency for assertion

--- src/test/java/com/bab/challenge/web/SignUpTests.java
+++ src/test/java/com/bab/challenge/web/SignUpTests.java
@@ -4,14 +4,18 @@ import com.bab.core.base.BaseTest;
 import com.bab.domain.page_object.AccountCreationPage;
 import com.bab.domain.page_object.AuthenticationPage;
 import com.bab.domain.page_object.HomePage;
+import com.bab.domain.page_object.MyAccountPage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.UUID;
 
 public class SignUpTests extends BaseTest {
 
     HomePage homePage;
     AuthenticationPage authenticationPage;
     AccountCreationPage accountCreationPage;
+    MyAccountPage myAccountPage;
 
     @BeforeMethod
     public void setUp() {
@@ -19,16 +23,18 @@ public class SignUpTests extends BaseTest {
         homePage = new HomePage();
         authenticationPage = new AuthenticationPage();
         accountCreationPage = new AccountCreationPage();
+        myAccountPage = new MyAccountPage();
     }
 
     @Test
     public void signUpTest() {
         homePage.openAndValidate();
         homePage.goToAuthenticationPage();
-        authenticationPage.enterEmailForAccountCreation("test41414@gmail.com");
+        authenticationPage.enterEmailForAccountCreation(UUID.randomUUID().toString() + "@gmail.com");
         authenticationPage.submitEmailRegistration();
         accountCreationPage.fillOutPersonalInformation("Anirban", "Dan", "testpass", "15", "2", "1995");
         accountCreationPage.fillOutAddressInformation();
         accountCreationPage.submitAccountCreation();
+        myAccountPage.assertMyAccountPage();
     }
 }
