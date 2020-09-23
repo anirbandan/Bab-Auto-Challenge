54c2420a8137bc99e480e8439da31388c1c7e624
adan <Viv_020922200791>
2020-09-23 17:20:25 +0200

    added test steps to fill personal and address information in account creation test case

--- src/test/java/com/bab/challenge/web/SignUpTests.java
+++ src/test/java/com/bab/challenge/web/SignUpTests.java
@@ -1,6 +1,8 @@
 package com.bab.challenge.web;
 
 import com.bab.core.base.BaseTest;
+import com.bab.domain.page_object.AccountCreationPage;
+import com.bab.domain.page_object.AuthenticationPage;
 import com.bab.domain.page_object.HomePage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,15 +10,25 @@ import org.testng.annotations.Test;
 public class SignUpTests extends BaseTest {
 
     HomePage homePage;
+    AuthenticationPage authenticationPage;
+    AccountCreationPage accountCreationPage;
 
     @BeforeMethod
     public void setUp() {
         super.setDriver();
         homePage = new HomePage();
+        authenticationPage = new AuthenticationPage();
+        accountCreationPage = new AccountCreationPage();
     }
 
     @Test
     public void signUpTest() {
         homePage.openAndValidate();
+        homePage.goToAuthenticationPage();
+        authenticationPage.enterEmailForAccountCreation("test41414@gmail.com");
+        authenticationPage.submitEmailRegistration();
+        accountCreationPage.fillOutPersonalInformation("Anirban", "Dan", "testpass", "15", "2", "1995");
+        accountCreationPage.fillOutAddressInformation();
+        accountCreationPage.submitAccountCreation();
     }
 }
